fd910190ce410329cb26bb5807341361645b6677
John <ashton.basile@gmail.com>
2023-09-28 13:14:04 +0100

    post request

--- pirateservice/src/main/java/com/example/pirateservice/controllers/PirateController.java
+++ pirateservice/src/main/java/com/example/pirateservice/controllers/PirateController.java
@@ -3,9 +3,7 @@ package com.example.pirateservice.controllers;
 import com.example.pirateservice.models.Pirate;
 import com.example.pirateservice.repositories.PirateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,5 +21,10 @@ public class PirateController {
     @GetMapping(value = "/pirates/{id}")
     public Optional<Pirate> getPirate(@PathVariable Long id) {
         return pirateRepository.findById(id);
+    }
+
+    @PostMapping(value = "/pirates")
+    public Pirate createPirate(@RequestBody Pirate pirate) {
+        return pirateRepository.save(pirate);
     }
 }
