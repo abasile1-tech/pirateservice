1d2537086d9595d0291f2a08d382b4c33b971149
John <ashton.basile@gmail.com>
2023-09-28 10:24:16 +0100

    added some annotations

--- pirateservice/src/main/java/com/example/pirateservice/models/Pirate.java
+++ pirateservice/src/main/java/com/example/pirateservice/models/Pirate.java
@@ -1,10 +1,19 @@
 package com.example.pirateservice.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pirates")
 public class Pirate {
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "age")
     private int age;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Pirate(String firstName, String lastName, int age) {
