24a1d77e033d1b6d78182c192183c207f2c98881
John <ashton.basile@gmail.com>
2023-09-28 10:40:50 +0100

    saved Pirate jack in the database

--- pirateservice/src/test/java/com/example/pirateservice/PirateserviceApplicationTests.java
+++ pirateservice/src/test/java/com/example/pirateservice/PirateserviceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.pirateservice;
 
+import com.example.pirateservice.models.Pirate;
+import com.example.pirateservice.repositories.PirateRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PirateserviceApplicationTests {
 
+	// Dependency Injection via Autowired
+	@Autowired
+	PirateRepository pirateRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void createPirate() {
+		Pirate jack = new Pirate("Jack", "Sparrow", 32);
+		pirateRepository.save(jack);
+	}
+
+
 }
+
